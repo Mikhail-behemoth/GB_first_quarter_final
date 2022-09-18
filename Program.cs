a249a3eae969221af1f28abc0dfb750c38d21e7e
Mikhail <rednex05@yandex.ru>
2022-09-18 12:49:17 +0300

    2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = new string[6] {"pie", "may", "granite", "012", "crystal", "crystal"};
+
+void ArrayThreeSymbols(string[] arr)
+{
+    string[] arr2 = new string[arr.Length];
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+    if(arr[i].Length <= 3)
+        {
+        arr2[count] = arr[i];
+        count++;
+        }
+    }
+for (int i = 0; i < arr2.Length; i++)
+    {
+        Console.Write($"{arr2[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+ArrayThreeSymbols(array);
